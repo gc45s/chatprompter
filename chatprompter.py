8caad147586b6c79f599332e5b91a0b0b744fa7b
gc45s <gurrycurry45@gmail.com>
2025-09-29 20:25:40 +0700

    Update chatprompter.py

--- chatprompter.py
+++ chatprompter.py
@@ -1,20 +1,3 @@
-#import streamlit as st
-#from transformers import pipeline, set_seed
-
-# Load GPT-2 generator
-#generator = pipeline('text-generation', model='gpt2')
-#set_seed(42)
-
-#st.title("Auto Text Prompter with GPT-2")
-
-#prompt = st.text_input("Masukkan awal kalimat:", "")
-
-#if prompt:
-#    results = generator(prompt, max_length=50, num_return_sequences=3)
-#    st.subheader("Hasil Prediksi:")
-#    for i, res in enumerate(results):
-#        st.markdown(f"**{i+1}.** {res['generated_text']}")
-
 import streamlit as st
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, set_seed
 
@@ -38,3 +21,4 @@ if prompt:
     results = generator(prompt, max_length=50, num_return_sequences=3)
     for i, res in enumerate(results):
         st.markdown(f"**{i+1}.** {res['generated_text']}")
+
